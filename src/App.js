8b7223e179f43a456b455d9aa17959216ffbefd3
Augustus Ellerm <ellermaugustus@gmail.com>
2021-11-16 23:18:02 +1300

    Added monaco_editor for native code editing. Architecture changed -> moved all react components into components folder. Started on implementing a file manager for code editor.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import {HashRouter,Route,Routes} from "react-router-dom";
+import GraphViewer from './components/graph_viewer';
+import CWLGenerator from './components/cwl_generator';
+import CodeViewer from './components/code_viewer';
+import Home from './components/home';
 
 // Required to use window.require as require() for some reason conflicts
-const electron = window.require('electron');
-const ipcRenderer = electron.ipcRenderer;
+// const electron = window.require('electron');
 
-function App() {
+const App = (props)=> {
   return (
+
+    <HashRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={()=>{
-          ipcRenderer.send("general-error")
-        }} >Wow</button>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/CodeViewer" element={<CodeViewer/>}/>
+        <Route exact path="/CWLGenerator" element={<CWLGenerator/>}/>
+        <Route exact path="/GraphViewer" element={<GraphViewer/>}/>
+      </Routes>   
     </div>
+    </HashRouter>    
   );
 }
 
