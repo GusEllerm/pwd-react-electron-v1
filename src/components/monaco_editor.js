466960cc5afe731694fb84a38e96bbb2f79458af
Augustus Ellerm <ellermaugustus@gmail.com>
2021-12-05 11:55:22 +1300

    Moving to new metal

--- src/components/monaco_editor.js
+++ src/components/monaco_editor.js
@@ -2,24 +2,15 @@ import React from 'react'
 import Editor from '@monaco-editor/react'
 
 function MonacoEditor () {
-    return (
-        <Editor
-            height="100vh"
-            width="100%"
-            language="json"
-            theme="vs-dark"
-            value='{"menu": {
-                "id": "file",
-                "value": "File",
-                "popup": {
-                  "menuitem": [
-                    {"value": "New", "onclick": "CreateNewDoc()"},
-                    {"value": "Open", "onclick": "OpenDoc()"},
-                    {"value": "Close", "onclick": "CloseDoc()"}
-                  ]
-                }
-              }}' />
-    )
+  return (
+    <Editor
+      height="100vh"
+      width="100%"
+      language="javascript"
+      theme="vs-dark"
+      defaultValue="// Some Comment"
+    />
+  )
 }
 
 export default MonacoEditor;
